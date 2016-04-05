97be30bd528089315430d2f3a4f0cc2bb4bb0030
John Keisker <jkeisker@me.com>
2016-04-05 16:23:01 -0500

    first commits

--- app/modules/characters/controller.js
+++ app/modules/characters/controller.js
@@ -46,10 +46,33 @@ class CharactersController {
 
 	constructor($http, $stateParams) {
     this._$http = $http;
+		this.name = $stateParams.name;
+		this.getData();
 	}
 
   getData() {
+		this._$http
+			.get(`http://gateway.marvel.com:80/v1/public/characters?name=${this.name}&apikey=0fd87f40d6dd1419e4153fe1a1c9cf04`)
+			.then((response) => {
+				console.log(response);
+				this.id = response.data.data.results[0].id;
+				this.description = response.data.data.results[0].description;
+				this.image = `${response.data.data.results[0].thumbnail.path}.${response.data.data.results[0].thumbnail.extension}`;
+
+
+				this._$http
+					.get(`http://gateway.marvel.com/v1/public/characters/${this.id}/events?apikey=0fd87f40d6dd1419e4153fe1a1c9cf04`)
+					.then((response) => {
+						console.log(response);
+						this.events = response.data.data.results;
+					})
+			})
+
   }
+
+	eventImage(event) {
+		return `${event.thumbnail.path}.${event.thumbnail.extension}`;
+	}
 
 }
 
